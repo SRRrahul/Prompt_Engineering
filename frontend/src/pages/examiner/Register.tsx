033273843e8c356c9108decac8448cc61b1089df
SRRrahul <srahu@gtec.edu>
2026-07-15 14:44:31 +0530

    Remove 'Other' option from Year dropdown

--- frontend/src/pages/examiner/Register.tsx
+++ frontend/src/pages/examiner/Register.tsx
@@ -187,7 +187,6 @@ export default function ExaminerRegister() {
                   <option value="2nd Year">2nd Year</option>
                   <option value="3rd Year">3rd Year</option>
                   <option value="4th Year">4th Year</option>
-                  <option value="Other">Other</option>
                 </select>
               </div>
 
